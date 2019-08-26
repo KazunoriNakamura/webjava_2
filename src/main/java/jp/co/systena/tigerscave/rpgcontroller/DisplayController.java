4a416c6d129c77fbb97626e0dab10983d5e735bc
systena <systena@PCK-137-270>
2019-08-26 22:45:39 +0900

    Add CreateForm

--- src/main/java/jp/co/systena/tigerscave/rpgcontroller/DisplayController.java
+++ src/main/java/jp/co/systena/tigerscave/rpgcontroller/DisplayController.java
@@ -2,33 +2,35 @@ package jp.co.systena.tigerscave.rpgcontroller;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import jp.co.systena.tigerscave.rpgview.CharacterForm;
 
+@Controller // Viewあり。Viewを返却するアノテーション
 public class DisplayController {
 
   // トップページ
-  @RequestMapping(value = "/create")
-  public String index(Model model) {
+  @RequestMapping(value = "/create", method = RequestMethod.GET) // URLとのマッピング
+  public ModelAndView index(ModelAndView  mav) {
 
       // ラジオボタンの選択肢
       Map<String, String> jobMap = new LinkedHashMap<String, String>();
-      jobMap.put("key_java", "戦士");
-      jobMap.put("key_python", "魔法使い");
+      jobMap.put("key_fighter", "戦士");
+      jobMap.put("key_magician", "魔法使い");
 
-      model.addAttribute("jobMapItems",jobMap);
+      mav.addObject("jobMapItems",jobMap);
 
-      // ビューの表示時にインスタンスを渡さないとエラーが出るので、その対策
-      model.addAttribute("characterForm", new CharacterForm());
-      return "index";
+      mav.addObject("jobForm", new CharacterForm());
+      return mav;
   }
 
-//確認ページ
+//コマンドページ
   @RequestMapping(value = "/command", method = RequestMethod.GET)
-  public String confirm(@ModelAttribute CharacterForm characterForm, Model model) {
-      return "confirm";
+  public String command(@ModelAttribute CharacterForm jobForm, Model model) {
+      return "command";
   }
 }
